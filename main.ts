0c0b866e7f0f626f3e71a09831100d213fb19a81
rlichfield <92386608+rlichfield@users.noreply.github.com>
2022-04-04 08:36:23 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,15 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Yes)
     music.stopAllSounds()
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . . . #
+        . # # # .
+        . . . . .
+        `)
+})
 music.playMelody("E B C5 A B G A F ", 120)
 basic.forever(function () {
     basic.showIcon(IconNames.Square)
